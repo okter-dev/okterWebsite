3bf58c0476fc44b3211902c7286873ae5e67e8c4
Baudry Souvignet <76646601+baudrysouvignet@users.noreply.github.com>
2023-03-12 15:59:03 +0100

    Desktop Ok (no anim no responsive)

--- index.php
+++ index.php
@@ -1,1 +1,63 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>okter</title>
+    <meta name="description" content="Créer un site web de qualité en Haute-Loire ? Notre équipe d'experts en développement web offre des solutions sur mesure pour les particuliers et les entreprises, y compris les PME. Nous sommes spécialisés dans la création de sites web hautement performants et optimisés pour les moteurs de recherche. Contactez-nous dès maintenant pour un site web professionnel et efficace."> <!--description sous la recherche Google-->
+    <meta name="author" content="Baudry Souvignet">
+    <meta name="author" content="okter">
+
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/home.css">
+
+    <link rel="stylesheet" href="https://use.typekit.net/xur3ymq.css">
+</head>
+<body>
+<section class="grid">
+    <article class="block title">
+        <h1 class="big-text">Nous proposons bien plus qu’un site</h1>
+        <p>Nous nous efforcons à créer des expériences uniques</p>
+    </article>
+    <div class="img"></div>
+
+    <article class="block black">
+        <h2 class="big-text">Des sites concu pour vous, pensez pour tous</h2>
+    </article>
+
+    <article class="block text">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+    </article>
+
+    <article class="block logo">
+        <img src="img/logo80.png" alt="logo de la société">
+    </article>
+
+    <ul class="block tag">
+        <li>Developper</li>
+        <li>Design</li>
+        <li>Experience utilisateur</li>
+        <li>Unique</li>
+        <li>🇫🇷</li>
+        <li>Pour vous</li>
+        <li>Okter</li>
+        <li>Personnalisé</li>
+        <li>Simple</li>
+    </ul>
+
+    <nav class="block nav">
+        <a>
+            <h3 class="big-text">Qui somme nous ?</h3>
+            <img src="icons/bigarrow.svg" alt="Fleche pour continuer">
+        </a>
+        <a>
+            <h3 class="big-text">Un site Web ?</h3>
+            <img src="icons/bigarrow.svg" alt="Fleche pour continuer">
+        </a>
+        <a>
+            <h3 class="big-text">Nous contacter !</h3>
+            <img src="icons/bigarrow.svg" alt="Fleche pour continuer">
+        </a>
+    </nav>
+</section>
+</body>
+</html>
